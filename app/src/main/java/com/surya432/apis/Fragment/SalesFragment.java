4154dc9d48537bba1e9485393c7f68f14bf85d36
surya432 <hadisurya295@gmail.com>
2019-03-12 09:45:34 +0700

    Sales Realisasi Design

--- app/src/main/java/com/surya432/apis/Fragment/SalesFragment.java
+++ app/src/main/java/com/surya432/apis/Fragment/SalesFragment.java
@@ -41,7 +41,7 @@ public class SalesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_sales, container, false);
         ButterKnife.bind(this, view);
         sessionManager = new SessionManager(getContext());
         firstLoad();
